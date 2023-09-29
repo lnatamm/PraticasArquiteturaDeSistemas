6c28fee5999096c0916fb1e69147e40bdc689279
Levi <lnatamm25@gmail.com>
2023-09-29 01:08:25 -0300

    Implementando as classe StateClass com a prática do padrão comportamental State

---
+++ State/StateClass.java
@@ -0,0 +1,93 @@
+package State;
+
+class PlayingState implements ReprodutionState{
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
+
+class PausingState implements ReprodutionState{
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
+
+class ResumingState implements ReprodutionState{
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
+
+class StoppingState implements ReprodutionState{
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
